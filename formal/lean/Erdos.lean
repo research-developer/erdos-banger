7e17d21d756a1a20df7eece40c002546a124c1f0
The-Obstacle-Is-The-Way <jj@novamindnyc.com>
2026-01-17 14:32:45 -0500

    chore: add lean project scaffolding from spec-007

---
+++ formal/lean/Erdos.lean
@@ -0,0 +1,7 @@
+-- Erdos.lean
+-- Root module that imports all problem formalizations
+
+import Erdos.Basic
+-- Individual problems imported as they're created:
+-- import Erdos.Problem006
+-- import Erdos.Problem067
